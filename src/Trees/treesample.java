52cc07857dfcef08c22864103f2fca252583d8eb
Utkarsh Kalia <utkarsh2kalia@gmail.com>
2020-08-26 00:16:47 +0530

    Initial commit

---
+++ src/Trees/treesample.java
@@ -0,0 +1,9 @@
+package Trees;
+
+import java.util.function.DoubleToIntFunction;
+
+public class treesample {
+    public static void main(String[] args) {
+        System.out.println("Raman");
+    }
+}
