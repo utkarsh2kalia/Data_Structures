6f415ab31a2dcbfdd374f9e268894c045c5bcd5c
Utkarsh Kalia <utkarsh2kalia@gmail.com>
2020-09-17 11:23:51 +0530

    Java Multithreading using Runnable interface

--- src/Java/MultithreadingusingRunnable.java
+++ src/Java/MultithreadingusingRunnable.java
@@ -1,4 +1,29 @@
 package Java;
 
-public class MultithreadingusingRunnable {
+public class MultithreadingusingRunnable implements Runnable {
+    public void run()
+    {
+        try
+        {
+            // Displaying the thread that is running
+            System.out.println ("Thread " +
+                    Thread.currentThread().getId() +
+                    " is running");
+
+        }
+        catch (Exception e)
+        {
+            // Throwing an exception
+            System.out.println ("Exception is caught");
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 8; // Number of threads
+        for (int i=0; i<n; i++)
+        {
+            Thread object = new Thread(new MultithreadingusingRunnable());
+            object.start();
+        }
+    }
 }
