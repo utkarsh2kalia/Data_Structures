29b63546e2af5d5ac313c920f75559e3180da36c
Utkarsh Kalia <utkarsh2kalia@gmail.com>
2020-10-05 16:11:08 +0530

    Kth Smallest Element in array MaxHeap O(nlog k)

---
+++ src/Arrays/KthSmallestElementInArrayusingMaxHeap.java
@@ -0,0 +1,35 @@
+package Arrays;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class KthSmallestElementInArrayusingMaxHeap {
+    public static int kthsmallestelem(List<Integer> list,int k){
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->b-a);
+
+        for(int i = 0;i<k; i++){
+            pq.add(list.get(i));
+        }
+        for(int i = k; i<list.size(); i++ ){
+            // check if the current element isl less than the top of queue. add to queue
+            if(list.get(i)<pq.peek())
+            {
+                pq.poll();
+                pq.add(list.get(i));
+            }
+
+        }
+        return pq.poll();
+
+
+    }
+
+    public static void main(String[] args) {
+        Integer arr[] = new Integer[]{2,1,3,1,4,5,3,2,59,977,64};
+//        List<Integer>list = Arrays.asList(arr);
+        System.out.println(kthsmallestelem(Arrays.asList(arr),6));
+    }
+}
--- src/Java/MinHeap.java
+++
@@ -1,9 +0,0 @@
-package Java;
-//
-//Arr[(i-1)/2]	Returns the parent node
-//        Arr[(2*i)+1]	Returns the left child node
-//        Arr[(2*i)+2]	Returns the right child node
-//The traversal method use to achieve Array representation is Level Order
-public class MinHeap {
-
-}
