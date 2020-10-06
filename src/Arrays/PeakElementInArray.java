ddefc5f40cc1f267d66181458e5924633fa74dce
Utkarsh Kalia <utkarsh2kalia@gmail.com>
2020-10-06 19:27:32 +0530

    Peak Element In array O(logn)

--- src/Arrays/PeakElementInArray.java
+++ src/Arrays/PeakElementInArray.java
@@ -4,7 +4,7 @@ public class PeakElementInArray {
     public static int findPeakElement(int[] nums) {
         int l = 0, r = nums.length - 1;
         while (l < r) {
-            int mid = (l + r) / 2;
+            int mid = l + (r - l) / 2;
             if (nums[mid] > nums[mid + 1])
                 r = mid;
             else
