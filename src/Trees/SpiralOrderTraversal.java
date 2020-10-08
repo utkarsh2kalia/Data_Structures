4125fe85e137c92fe4af12fecfee0c1280bbe351
Utkarsh Kalia <utkarsh2kalia@gmail.com>
2020-10-08 18:40:47 +0530

    Zigzag/Spiral order traversalO(n)

--- src/Trees/SpiralOrderTraversal.java
+++ src/Trees/SpiralOrderTraversal.java
@@ -1,6 +1,49 @@
 package Trees;
 
+import java.util.*;
+
+import static Trees.InorderTraversal.maketree;
+
 public class SpiralOrderTraversal {
 
+    public static List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
 
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        boolean reverse = false;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            /* use linkedlist to control time complexity of addFirst to O(1) */
+            LinkedList<Integer> tempList = new LinkedList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode curNode = queue.poll();
+                if (reverse) {
+                    tempList.addFirst(curNode.data);
+                } else {
+                    tempList.add(curNode.data);
+                }
+
+                if (curNode.left != null) {
+                    queue.offer(curNode.left);
+                }
+                if (curNode.right != null) {
+                    queue.offer(curNode.right);
+                }
+            }
+            result.add(tempList);
+            reverse = !reverse;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {3,2,1,3,4,5,6,54,3,31,2};
+        System.out.println(zigzagLevelOrder(maketree(arr)));
+    }
 }
