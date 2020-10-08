25be86e2198fe3bf28d2234fb79821691c4b773e
Utkarsh Kalia <utkarsh2kalia@gmail.com>
2020-10-09 00:19:27 +0530

    Diameter of a tree O(n) space and time

--- src/Trees/DiameterOfATree.java
+++ src/Trees/DiameterOfATree.java
@@ -20,23 +20,30 @@ public class DiameterOfATree {
 //    Note: The length of path between two nodes is represented by the number of edges between them.
 //max(depth of node.left, depth of node.right) + 1
 //
-static int max = 0;
-public static int diameteroftree(TreeNode root){
 
-    if(root == null)
-        return 0;
-    // keep moving left till we reach the leftmost node
-    int left = diameteroftree(root.left);
-    int right = diameteroftree(root.right);
-    max = Math.max(max, left+right);
-    return Math.max(left,right)+1;
+    int max = 0;
 
-}
+    public int diameterofBinaryTree(TreeNode root) {
+        maxDepth(root);
+        return max;
+    }
+
+    private int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        max = Math.max(max, left + right);
+
+        return Math.max(left, right) + 1;
+    }
     public static void main(String[] args) {
         int arr[] = {1,2,4,32,65,33,98,10};
         TreeNode root = maketree(arr);
-        System.out.println(zigzagLevelOrder(root));
-        System.out.println(diameteroftree(root));
+//        System.out.println(zigzagLevelOrder(root));
+
+        System.out.println((new DiameterOfATree()).diameterofBinaryTree(root));
     }
 
 }
