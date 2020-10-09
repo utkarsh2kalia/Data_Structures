fc7c41c258ca432e2742547c6162d32996f13811
Utkarsh Kalia <utkarsh2kalia@gmail.com>
2020-10-09 15:59:08 +0530

    Added 2nd method

--- src/Arrays/PeakElementInArray.java
+++ src/Arrays/PeakElementInArray.java
@@ -1,6 +1,7 @@
 package Arrays;
 
 public class PeakElementInArray {
+    // use binary search to find peak element O(logn)
     public static int findPeakElement(int[] nums) {
         int l = 0, r = nums.length - 1;
         while (l < r) {
@@ -12,9 +13,20 @@ public class PeakElementInArray {
         }
         return l;
     }
+    public static int findPeakElement2(int[] nums) {
+        if(nums.length == 1)
+            return 0;
 
+        for(int i = 0; i<nums.length-1; i++)
+            if(nums[i]>nums[i+1])
+                return i;
+        if(nums[nums.length-1]>nums[nums.length-2])
+            return nums.length-1;
+        return 0;
+    }
     public static void main(String[] args) {
-        int arr[] = {1,2,1,3,5,6,4};
+        int arr[] = {1, 2, 1, 3, 5, 6, 4};
         System.out.println("Index:"+findPeakElement(arr)+" ");
+        System.out.println("Index:"+findPeakElement2(arr)+" ");
     }
 }
