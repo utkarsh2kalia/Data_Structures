45e7cfc1c5b3218f68c1972262fd9e3fac445409
Utkarsh Kalia <utkarsh2kalia@gmail.com>
2020-10-09 15:34:20 +0530

    Count the number of set bits in an integer

---
+++ src/BitManipulation/CountSetBitsinInteger.java
@@ -0,0 +1,18 @@
+package BitManipulation;
+
+public class CountSetBitsinInteger {
+    public static int countsetbits(int n){
+        int count = 0;
+        while(n!=0){
+            count += (n & 1);// check if last bit is 1
+            n >>= 1; // move bits to right
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int n = 16;
+        System.out.println(n+" in binary is "+ Integer.toBinaryString(n));
+        System.out.println("The number of set bits in "+n+ " is " +countsetbits(n));
+    }
+}
